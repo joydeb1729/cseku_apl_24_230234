67a5d9f9349153e4511fab67285c3562820f298e
joydeb1729 <joydebganprokas@gmail.com>
2024-11-03 22:40:14 +0600

    Main method fixed

--- Task2/footballClubManagementSystem/src/Main.java
+++ Task2/footballClubManagementSystem/src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Create a stadium
+
         Stadium stadium = new Stadium("Khulna Stadium", 50000, "City Center");
 
-        // Create a club
         Club club = new Club("Champions FC", stadium, "Khulna");
 
-        // Create a coach
         Coach coach = new Coach("Ancelotti", 10, "Offensive", 80000);
 
-        // Create teams
         Team seniorTeam = new Team("Senior Team", coach, "4-4-2", "Red and White");
         Team juniorTeam = new Team("Junior Team", coach, "4-3-3", "Blue and White");
 
-        // Create players
         Player seniorPlayer1 = new SeniorPlayer("Rahul", 28, "Forward", 50000, "Female", 5, 90, 25);
         Player seniorPlayer2 = new SeniorPlayer("Durjay", 30, "Midfielder", 55000, "Male", 4, 80, 20);
         Player juniorPlayer1 = new JuniorPlayer("Karim", 18, "Defender", 20000, "Male", 2, "St Josephs High School", false);
@@ -36,13 +32,13 @@ public class Main {
         club.addRevenue(2600000);
         System.out.println("Balance after revenue: " + club.getBalance());
 
-        // Pay salaries
+
         club.paySalaries();
         System.out.println("Balance after paying salaries: " + club.getBalance());
 
         // Record match results for teams
-        seniorTeam.recordMatchResult(true); // Senior team wins
-        juniorTeam.recordMatchResult(false); // Junior team loses
+        seniorTeam.recordMatchResult(true); 
+        juniorTeam.recordMatchResult(false);
 
         // Output team performance
         System.out.println(seniorTeam.getTeamPerformance());
